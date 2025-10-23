8ab1770183f7535b114cc2ff22c2bcd718313827
Edmo Lima <soedmolima@gmail.com>
2025-10-23 17:05:08 +0200

    add domains

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TenancyModule } from './tenancy/tenancy.module';
+import { LakeModule } from './lake/lake.module';
+import { InsightsModule } from './insights/insights.module';
+import { IntelligenceModule } from './intelligence/intelligence.module';
+import { EtlModule } from './etl/etl.module';
+import { CommonModule } from './common/common.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [TenancyModule, LakeModule, InsightsModule, IntelligenceModule, EtlModule, CommonModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
