46008a31b114acf5f0f628b34d8c383c2c7cd76a
Edmo Lima <soedmolima@gmail.com>
2025-10-23 17:15:31 +0200

    chore: update dataqore modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TenancyModule } from './tenancy/tenancy.module';
-import { LakeModule } from './lake/lake.module';
-import { InsightsModule } from './insights/insights.module';
-import { IntelligenceModule } from './intelligence/intelligence.module';
-import { EtlModule } from './etl/etl.module';
+import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
-import { AuthModule } from './auth/auth.module';
+import { EtlModule } from './etl/etl.module';
+import { InsightsModule } from './insights/insights.module';
+import { IntelligenceModule } from './intelligence/intelligence.module';
+import { LakeModule } from './lake/lake.module';
+import { TenancyModule } from './tenancy/tenancy.module';
 
 @Module({
-  imports: [TenancyModule, LakeModule, InsightsModule, IntelligenceModule, EtlModule, CommonModule, DatabaseModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TenancyModule,
+    LakeModule,
+    InsightsModule,
+    IntelligenceModule,
+    EtlModule,
+    CommonModule,
+    DatabaseModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
